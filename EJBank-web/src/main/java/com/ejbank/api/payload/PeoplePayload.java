d377606e49e9458d260b95367019f828a3351e5f
mickael.boudignot <mickael.boudignot@rakuten.com>
2025-11-25 10:20:47 +0100

    Patch payload for post test

--- EJBank-web/src/main/java/com/ejbank/api/payload/PeoplePayload.java
+++ EJBank-web/src/main/java/com/ejbank/api/payload/PeoplePayload.java
@@ -2,9 +2,12 @@ package com.ejbank.api.payload;
 
 public class PeoplePayload {
     
-    private final String firstname;
-    private final String lastname;
-    private final Integer age;
+    private String firstname;
+    private String lastname;
+    private Integer age;
+
+    public PeoplePayload() {
+    }
 
     public PeoplePayload(String firstname, String lastname, Integer age) {
         this.firstname = firstname;
@@ -22,5 +25,17 @@ public class PeoplePayload {
 
     public Integer getAge() {
         return age;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
